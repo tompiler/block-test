903204eee0de405c06593fe0ed8276fc6915cc22
tompiler <tomlynch3091341@googlemail.com>
2019-06-09 19:04:06 +0100

    implemented an adjacentColour function to return all adjacent blocks of the same colour, given an initial block

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -6,7 +6,7 @@ export class Block {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.id = `${x}x${y}`; // added id variable
+    this.id = `${x}x${y}`;
     this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
   }
 }
@@ -52,12 +52,72 @@ export class BlockGrid {
     return this;
   }
 
+  getBlock([x, y]) {
+    return this.grid[x] &&
+      this.grid[x].find(block => {
+        return block.y === y;
+      });
+  }
+
+  adjacentColour(block) {
+    // given a block, return all adjacent blocks of the same colour
+
+    console.log("BLOCK:", block);
+    const { x, y, colour, id, active } = block;
+
+    const leftBlock = this.getBlock([x - 1, y]);
+    const rightBlock = this.getBlock([x + 1, y]);
+    const upBlock = this.getBlock([x, y + 1]);
+    const downBlock = this.getBlock([x, y - 1]);
+
+    console.log(leftBlock, rightBlock, upBlock, downBlock);
+    const adjacents = [block];
+    if (x - 1 >= 0 && leftBlock && leftBlock.colour === colour) {
+      adjacents.push(leftBlock);
+    }
+    if (x + 1 < MAX_X && rightBlock && rightBlock.colour === colour) {
+      adjacents.push(rightBlock);
+    }
+    if (y - 1 >= 0 && downBlock && downBlock.colour === colour) {
+      adjacents.push(downBlock);
+    }
+    if (y + 1 < MAX_Y && upBlock && upBlock.colour === colour) {
+      adjacents.push(upBlock);
+    }
+    // console.log(...adjacents);
+    return adjacents;
+  }
+
   blockClicked(e, block) {
     console.log("BLOCK:", block);
+    var adjacents = this.adjacentColour(block);
+    // var IDs = new Set(adjacents.map(d => d.id));
+    console.log("Adjacents:", adjacents);
+    for (var i = 0; i < adjacents.length; i++) {
+      var newAdjacents = this.adjacentColour(adjacents[i]);
+      // newAdjacents.map(d => d.id).forEach(d => IDs.add(d));
+      // var newIDs = newAdjacents.map(d => d.id);
+      // console.log("NewAdjacents:", newAdjacents);
+      newAdjacents.forEach(d => {
+        var IDs = adjacents.map(adjacent => adjacent.id);
+        if (!IDs.includes(d.id)) {
+          adjacents.push(d);
+        }
+      });
+    }
 
-    var columnDiv = document.getElementById(`col_${block.x}`);
-    var rowDiv = document.getElementById(`block_${block.id}`);
-    columnDiv.removeChild(rowDiv);
+    // var columnDiv = document.getElementById(`col_${block.x}`);
+    // var rowDiv = document.getElementById(`block_${block.id}`);
+    // rowDiv.className += "-removed";
+    // // if (active) {
+    // setTimeout(
+    //   () => {
+    //     columnDiv.removeChild(rowDiv);
+    //   },
+    //   0
+    // );
+
+    console.log("Adjacents:", adjacents);
   }
 }
 
